6e5414effe8a3db1fe24ade9261b845c96a7d6b5
Tanzeb Choudhury <Tanzeem123456@gmail.com>
2022-05-21 01:33:48 -0400

    added media queries

--- src/head.js
+++ src/head.js
@@ -5,21 +5,17 @@ function header() {
     element.classList.add('hero');
 
     element.innerHTML = `
+    <div class="shape first"></div>
+    <div class="shape second"></div>
+    <div class="shape third"></div>
+    <div class="shape fourth"></div>
     <div class="text-container">
         <div class="text" id="intro">Hi I'm</div>
         <div class="text" id="fullName">Tanzeem Xhidori</div>
+        <div class="text" id="title">Web Developer</div>
     </div>`;
 
-    var intro =  document.getElementById('intro');
-    var fullName =  document.getElementById('fullName');
-    var floatIntro = setTimeout(function(){
-        intro.classList.add('floating');
-    }, 1000);
     
-    var floatName = setTimeout(function(){
-        fullName.classList.add('floating')
-         console.log('djfldksj')
-    }, 1900);
 
     return element;
 
@@ -28,14 +24,18 @@ function header() {
 function addAnimation(){
     let intro =  document.getElementById('intro');
     let fullName =  document.getElementById('fullName');
+    let title =  document.getElementById('title');
     let floatIntro = setTimeout(function(){
         intro.classList.add('floating');
     }, 1000);
     
     let floatName = setTimeout(function(){
         fullName.classList.add('floating')
-         console.log('djfldksj')
     }, 1900);
+    let floatTitle = setTimeout(function(){
+        title.classList.add('floating')
+         
+    }, 2800);
 }
 
 export  {header, addAnimation}
--- src/projects.js
+++ src/projects.js
@@ -12,9 +12,34 @@ function eyeSpy(){
   <div class="details">
     <h1 class='project-title'>Eye Spy</h1>
     <div class="icon-container">
-      
+    <div class="icon">
+    <a href="">
+      <img src="https://img.icons8.com/ios-filled/344/github.png" alt="Github">
+    </a>
+  </div>
+  <div class="icon">
+    <a href="">
+      <img src="https://img.icons8.com/ios/344/domain.png" alt="Github">
+    </a>
+  </div>
     </div>
-    <p>Hello</p>
+    <p class="tools">Tools Used:</p>
+    <div class="tool-container">
+    <div class="icon">
+      <img src="https://img.icons8.com/color/344/html-5--v1.png" alt="HTML"> 
+  </div>
+  <div class="icon">
+      <img src="https://img.icons8.com/color/344/css3.png" alt="CSS">
+  </div>
+  <div class="icon">
+      <img src="https://img.icons8.com/color/344/javascript--v1.png" alt="JavaScript">
+  </div>
+  <div class="icon">
+      <img src="https://img.icons8.com/color/344/webpack.png" alt="Webpack">
+  </div>
+  <div class="icon">
+      <img src="https://img.icons8.com/color/344/google-firebase-console.png" alt="Firebase">
+  </div>
   </div>`
 
   return eyeSpy;
